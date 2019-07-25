9765e16b5f3bc9ce2520a6bc57533958d7f776fd
od57 <Anshul.Aman@tesco.com>
2019-07-25 22:40:53 +0530

    Phase 1 done added images of events

---
+++ app/src/main/java/com/example/campediav1/EventsList.java
@@ -0,0 +1,18 @@
+package com.example.campediav1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventsList extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
